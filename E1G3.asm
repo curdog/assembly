dfeddd3d44436010775caf6c02079575f91b7920
curdog <curdog@gmail.com>
2015-02-18 17:51:24 -0500

    Filling in code

--- E1G3.asm
+++ E1G3.asm
@@ -2,31 +2,61 @@ TITLE Group 3 Exam 1 Program
 ;Exam 1 group 3 program
 ;
 ;
+Include Irvine32.inc
 
 .386
 .model flat stdcall
 
 .data
   buffsize byte 80
-  inBuff byte buffsize dup(0)
+  buffer byte buffsize dup(0)
   acc dword
-
+  promptbeg "Enter numbers separated by spaces Q to exit",0
+  prompterrc "Bad char",0
 .code
-;friendly message
-
-
+main PROC
 ;loop start
 LOOP: nop
+;friendly message
+  mov edx, offset promptbeg
+  call WriteString
+
 ;read input
-
+  mov edx, OFFSET buffer
+  mov ecx, sizeof buffer
+  call ReadString
 ;jump quit
+  mov ebx, eax
+  mov eax,0
+  sahf
+  cmp ebx,1
+  lahf
+  mov ecx,ah
+  cmp buffer[edi],'Q'
+  lahf
+  mov al,ah
+  mov ah,0
+  
+  
+;more data
 
-;process number
+;process number loop
 
 
 ;add into acc
 
-;clear array
 
+;print nmber
+
+;clear array
+  mov edi, 0
+CLRA: nop
+  mov buffer[edi],0
+  add edi,1
+  cmp edi, 80
+  jne CLRA
 ;quit
-END: nop
+ENDL: nop
+exit
+main ENDP
+END main
