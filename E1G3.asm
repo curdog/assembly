af00541247a0dff39c3f79883fcd5196e63087c8
curdog <curdog@gmail.com>
2015-02-18 15:59:42 -0500

    Sudo Code Outline
    Outline in comments for code

--- E1G3.asm
+++ E1G3.asm
@@ -7,5 +7,26 @@ TITLE Group 3 Exam 1 Program
 .model flat stdcall
 
 .data
+  buffsize byte 80
+  inBuff byte buffsize dup(0)
+  acc dword
 
 .code
+;friendly message
+
+
+;loop start
+LOOP: nop
+;read input
+
+;jump quit
+
+;process number
+
+
+;add into acc
+
+;clear array
+
+;quit
+END: nop
