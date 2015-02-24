311fd1191a2aba06e9b47270d0bc898b80c6c413
John Kirschner <kir2898@calu.edu>
2015-02-24 12:15:47 -0500

    leedle leedle
    Fixed views, clears, added max's code for rollu, messed around with popfunc

--- G3P2.asm
+++ G3P2.asm
@@ -1,6 +1,6 @@
-TITLE Assembly Program 1  by Group 5
+TITLE Assembly Program 1  by Group 3
 
-; Description:    Assembly Program 2
+; Description:    Assembly Program 1
 ; Class:          CSC
 ; Members:        Sean Curtis, Max Conroy, John Kirshner
 ; Revision date:  2/23
@@ -14,40 +14,39 @@ STACKDATASIZE equ 4
 CR equ 0Dh
 LF equ 0Ah
 buffersize equ 41
-dstack dword 8 DUP(0)
-tempstack dword 8 DUP(0) ;for rolling
 
+dstack dword 8 DUP(0)
+tempstack dword 8 DUP(0)				;for rolling
 buffer byte buffersize dup(0)
 promptMenu byte "Enter an input to add onto the stack",LF,CR,
- "+ - * /: relative mathematical operations",LF,CR,
-  "X: exchange top two elements of the stack",LF,CR,
-   "N: negate top element of stack",LF,CR,
-    "U: roll the stack up",LF,CR,
+	"+ - * /: relative mathematical operations",LF,CR,
+	"X: exchange top two elements of the stack",LF,CR,
+	"N: negate top element of stack",LF,CR,
+	"U: roll the stack up",LF,CR,
 	"D: roll stack down",LF,CR,
-	 "V: view all 8 elements of the stack",LF,CR,
-	  "C: clear the stack",LF,CR,
-	   "Q: quit the program",LF,CR,0
+	"V: view all 8 elements of the stack",LF,CR,
+	"C: clear the stack",LF,CR,
+	"Q: quit the program",LF,CR,0
 
 promptInvalid byte "Invalid input",LF,CR,0
+promptError byte "Error processing request",LF,CR,0
 charred dword 0
-shead dword 0
+shead dword -1
 .code
 
 ;
 ;main procedure
 ;
 main PROC
-;title/desc
-	call Clrscr
 ;start loop
 Begin:
 	mov edx, offset promptMenu
-	call WriteString
+	call WriteString					;display the input menu
 
 	mov edx, OFFSET buffer
 	mov ecx, sizeof buffer
-	call ReadString
-	mov charred,eax		;get input in eax
+	call ReadString	
+	mov charred,eax						;get user input in eax
 	movzx eax, buffer[0]
 	cmp eax,'q'
 	je Fin
@@ -71,11 +70,6 @@ PushNumber:
 ;keep the program open so user has the time to 
 ;think about what he has done. 
 Fin:
-	call Crlf
-	call WaitMsg
-	mov edx, offset buffer
-	mov ecx, sizeof buffer
-	call ReadString
 	INVOKE ExitProcess,0
 main ENDP
 
@@ -84,7 +78,22 @@ main ENDP
 ;result in eax
 ;
 popfunc PROC 
-	cmp shead, 0
+	push eax
+	push ebx
+
+	cmp shead,0
+	jl ERROR						;check if shead is 0 then error if its less then
+	mov eax, shead					;have to use eax because of imul
+	dec shead						;remember to decrement
+	imul eax,STACKDATASIZE
+	mov ebx,dstack[eax]				;store at temp register
+	mov dstack[eax],0				;clear contents of stack that we popped
+	mov eax,ebx						;will return eax
+	call WriteInt
+	jmp Cont
+
+	;
+	;cmp shead, 0
 	push edi
 	mov eax,shead
 	inc eax
@@ -102,11 +111,16 @@ popfunc PROC
 ERROR: nop
 	pop edi
 	;setc
+Cont:
+	pop ebx
+	pop eax
 	ret
 popfunc ENDP
 
+;
 ;push MACRO
 ;push in eax
+;
 pushs PROC
 	push edi
 	push eax
@@ -266,13 +280,13 @@ negs ENDP
 ;roll up macro
 ;
 rollu PROC
-push eax
-push ebx
-push esi
-mov esi,0
-mov eax,shead
-cmp eax,0
-je proces ;skip copytemp
+	push eax
+	push ebx
+	push esi
+	mov esi,0
+	mov eax,shead
+	cmp eax,0
+	je proces ;skip copytemp
 copy_temp: ;copy to temp
 	cmp esi,eax
 	jg rolltop ;jump when all but top is copied
@@ -294,10 +308,10 @@ copy_back: ;copy from temp to dstack
 	add esi,4
 	jmp copy_back
 cdone:
-pop esi
-pop ebx
-pop eax
-ret
+	pop esi
+	pop ebx
+	pop eax
+	ret
 rollu ENDP
 
 ;
@@ -313,20 +327,20 @@ rolld ENDP
 ;
 views PROC
 	push eax
-	push ebx
+	push esi
 
-	mov ebx,0
+	mov esi,0
 PopStack:
-	mov eax, ebx
+	mov eax, esi
 	imul eax,STACKDATASIZE		;calculate for the next esi result in eax
 	mov eax,dstack[eax]			;grab the contents at the index specified
 	call WriteInt				;display the contents of the stack
 	call Crlf
-	inc ebx
-	cmp eax,8					;are we at the end of the stack
+	inc esi
+	cmp esi,8					;are we at the end of the stack
 	jl PopStack
 
-	pop ebx
+	pop esi
 	pop eax
 	ret
 views ENDP
@@ -335,21 +349,20 @@ views ENDP
 ;clear stack macro
 ;
 clears PROC
-;push eax
-
 	push eax
-	push ebx
+	push esi
 
-	mov ebx,0
+	mov esi,0
 ClrStack:
-	mov eax, ebx
+	mov eax,esi
 	imul eax,STACKDATASIZE		;calculate for the next esi result in eax
 	mov dstack[eax],0			;grab the contents at the index specified
-	inc ebx
-	cmp eax,8					;are we at the end of the stack
+	inc esi
+	cmp esi,8					;are we at the end of the stack
 	jl ClrStack
+	mov shead,-1				;reset the stack index
 
-	pop ebx
+	pop esi
 	pop eax
 	ret
 clears ENDP
@@ -361,6 +374,17 @@ clears ENDP
 stahp PROC
 stahp ENDP
 
+;
+;dispError
+;displays that an error was encountered during processing
+;
+dispError PROC
+	push eax
+
+
+	pop eax
+dispError ENDP
+
 
 ;ckEqual
 ;checks to see if a value, in eax, is equal to the value inputted
@@ -369,7 +393,7 @@ ckEqual MACRO char, func
   cmp eax,char
   jnz nit
   call func
-  jmp fin
+  call main
   nit:
 ENDM
 
@@ -439,7 +463,6 @@ push edi
   jl PNLOOP
   mov eax, edx
   pop edx
-  
   jmp fin
 invalid:
   mov ebx,2
