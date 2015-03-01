6637d4aaa96192adb15ae2319f0814e569efa64b
John Kirschner <kir2898@calu.edu>
2015-03-01 11:24:54 -0500

    print
    added print macro which prints a string literal. Still can't figure out how to pass an existing string to a macro, therefore normStr still doesn't work. Will try to fix later

--- G3P2.asm
+++ G3P2.asm
@@ -10,496 +10,140 @@ Include Irvine32.inc
 .model flat, stdcall
 ExitProcess PROTO, dwExistCode:DWORD
 .data
-STACKDATASIZE equ 4
-CR equ 0Dh
-LF equ 0Ah
-buffersize equ 41
+	CR equ 0Dh
+	LF equ 0Ah
+	cmdQuit byte "quit",0
+	cmdHelp byte "help",0
+	cmdLoad byte "load",0
+	cmdRun byte "run",0
+	cmdKill byte "kill",0
+	cmdShow byte "show",0
+	cmdStep byte "step",0
+	cmdChange byte "change",0
+	inBuffer byte 30
+	helpMenu byte "Here are commands ,their parameters, and what they do",CR,LF,
+		"Quit: quits the program",CR,LF,
+		"Help: displays this help prmopt",CR,LF,
+		"Load <name> <priority> <run_time>: loads a program into memory",CR,LF,
+		"Run <name>: runs a program that has been loaded into memory",CR,LF,0
+	helpMenu2 byte "Hold <name>: holds up a program's processing, can continue later",CR,LF,
+		"Kill <name>: kills a program that is in hold mode",CR,LF,
+		"Show: shows processes and their status",CR,LF,
+		"Step <n>: steps n cycles to further jobs",CR,LF,
+		"Change <name> <priority>: changes the priority of a process",CR,LF,0
+	errorMsg byte "An error occured while processing your request",CR,LF,0
+	errorMsgInput byte "You have entered an invalid command",CR,LF,0
 
-dstack dword 8 DUP(0)
-tempstack dword 8 DUP(0)				;for rolling
-buffer byte buffersize dup(0)
-promptMenu byte "Enter an input to add onto the stack",LF,CR,
-	"+ - * /: relative mathematical operations",LF,CR,
-	"X: exchange top two elements of the stack",LF,CR,
-	"N: negate top element of stack",LF,CR,
-	"U: roll the stack up",LF,CR,
-	"D: roll stack down",LF,CR,
-	"V: view all 8 elements of the stack",LF,CR,
-	"C: clear the stack",LF,CR,
-	"Q: quit the program",LF,CR,0
-
-promptInvalid byte "Invalid input",LF,CR,0
-promptError byte "Error processing request",LF,CR,0
-charred dword 0
-shead dword -1
 .code
+;macros
+
+;
+;normStr
+;converts all characters in the string to lowercase
+;
+normStr MACRO str
+	.code
+	push edi
+	mov edi,-1
+normCont:
+	inc edi
+	cmp str[edi],5Ah		;compare to 'Z'
+	jg normCont				;is a lower case letter, or the ascii values that don't get manipulated, regardless, no action
+	cmp str[edi],41h		;compare to 'A'
+	jge normMask			;is an uppercase letter and needs masked
+	cmp str[edi],0			;is a null terminator 
+	je normFin
+	movzx eax,str[edi]		;;;;
+	call WriteInt
+	jmp normCont			;you aren't finished processing the string and need to continue
+normMask:
+	add str[edi],20h
+	jmp normCont
+normFin:
+	pop edi
+ENDM
+
+print MACRO str
+	LOCAL string
+	.data
+	string byte str,0
+	.code
+	push edx
+	mov edx, offset string
+	call WriteString
+	pop edx
+ENDM
+
+;procedures
+
+;
+;cmpString macro
+;compares two strings and executes an appropriate procedure if they are equal
+;
+cmpString MACRO str1,str2,func
+ENDM
+
+;
+;switchCmd
+;a switch case like statement that compares the
+;input buffer to valid commands
+;
+switchCmd PROC
+	;check for exit
+	cmpString inBuffer,quit,exitProgram
+switchCmd ENDP
+
+;
+;exitProgram
+;exits the program
+;put into a function because of check equality macro
+;
+exitProgram PROC
+	INVOKE ExitProcess,0
+exitProgram ENDP
+
+
+;
+;dispHelp procedure
+;prints out the help menu
+;shows each job, their parameters, and their purpose
+;
+dispHelp PROC
+	push edx
+	mov edx,offset helpMenu
+	call WriteString
+	mov edx,offset helpMenu2
+	call WriteString
+	pop edx
+	ret
+dispHelp ENDP
+
+;
+;dispErrorMsg
+;displays a general purpose error message
+;
+dispErrorMsg PROC
+	push edx
+	mov edx,offset errorMsg
+	call WriteString
+	pop edx
+	ret
+dispErrorMsg ENDP
 
 ;
 ;main procedure
 ;
 main PROC
-;start loop
-Begin:
-	mov edx, offset promptMenu
-	call WriteString					;display the input menu
-
-	mov edx, OFFSET buffer
-	mov ecx, sizeof buffer
-	call ReadString	
-	mov charred,eax						;get user input in eax
-	movzx eax, buffer[0]
-	cmp eax,'q'
-	je Fin
-	cmp eax,'Q'
-	je Fin
-
-	call checkOp		;test to see if it was a valid input
-
-	cmp ebx,2			;check to see if there was an invalid input
-	je ShowInvalid
-	cmp ebx,1
-	je PushNumber			;check for number
-	jmp Begin			;continue the loop until user exists
-ShowInvalid:
-	mov edx, offset promptInvalid
+MainStart:
+	call dispHelp
+	mov edx, OFFSET inBuffer		;set everything up for ReadString
+	mov ecx, sizeof inBuffer
+	call ReadString
+	print "Hello world"
+	normStr inBuffer				;normalize the inBuffer to all lowercase letters
+	mov edx, offset inBuffer
 	call WriteString
-	jmp Begin
-PushNumber:
-	call pushs
-	jmp Begin
-Fin:
-	INVOKE ExitProcess,0
-main ENDP
-
-;
-;pop procedure
-;result in eax
-;
-popfunc PROC
-	push ebx
-
-	cmp shead,0
-	jl ERROR						;check if shead is 0 then error if its less then
-	mov eax, shead					;have to use eax because of imul
-	dec shead						;remember to decrement
-	imul eax,STACKDATASIZE
-	mov ebx,dstack[eax]				;store at temp register
-	mov dstack[eax],0				;clear contents of stack that we popped
-	mov eax,ebx						;will return eax
-	jmp Cont
-ERROR: nop
-	call dispError
-Cont:
-	pop ebx
-	ret
-popfunc ENDP
-
-;
-;push MACRO
-;push in eax
-;
-pushs PROC
-	push edi
-	push eax
-	mov eax,shead
-	inc eax
-;check size
-	cmp eax, 8
-	jge ERROR
-	mov shead,eax
-	imul eax, STACKDATASIZE
-	mov edi,eax
-	pop eax
-	mov dstack[edi], eax
-	pop edi
-	clc
-	jmp ENDQ
-ERROR: nop
-	pop eax
-	pop edi
-	stc
-ENDQ:nop
-	ret
-pushs ENDP
-
-;
-;add function
-;
-adds PROC
-	push eax
-	push ebx
-	push edi
-
-	;check if there are at least two items in the stack
-	cmp shead,1
-	jl AddsFin
-	call popfunc
-	mov ebx,eax
-	call popfunc
-	add eax, ebx
-	call pushs
-
-AddsFin:
-	pop edi
-	pop ebx
-	pop eax
-	ret
-adds ENDP
-
-;
-;subs macro
-;
-subs PROC
-	push eax
-	push ebx
-	push edi
-
-	;check if there are at least two items in the stack
-	cmp shead,1
-	jl SubsFin
-	call popfunc
-	mov ebx,eax
-	call popfunc
-	sub eax, ebx
-	call pushs
-
-SubsFin:
-	pop edi
-	pop ebx
-	pop eax
-	ret
-subs ENDP
-
-;
-;div macro
-;
-divs PROC
-	push eax
-	push ebx
-	push ecx
-	push edx
-	push edi
-
-	;check if there are at least two items in the stack
-	cmp shead,1
-	jl DivsFin
-	;ready for idiv
-	mov edx, 0
-	call popfunc
-	mov ecx,eax
-	call popfunc
-	mov ebx,eax
-	mov eax, ecx
-	idiv ebx
-	call pushs
-
-DivsFin:
-	pop edi
-	pop edx
-	pop ecx
-	pop ebx
-	pop eax
-	ret
-divs ENDP
-
-;
-;mul macro
-;
-muls PROC
-	push eax
-	push ebx
-	push ecx
-	push edx
-	push edi
-
-	;check if there are at least two items in the stack
-	cmp shead,1
-	jl MulsFin
-	;ready for imul
-	call popfunc
-	mov edx,eax
-	call popfunc
-	imul eax,edx
-	call pushs
-	
-MulsFin:
-	pop edi
-	pop edx
-	pop ecx
-	pop ebx
-	pop eax
-	ret
-muls ENDP
-
-;
-;exch macro
-;
-exchs PROC
-	push eax
-	push ebx
-	push ecx
-	
-	mov eax,0
-	call popfunc
-	mov ebx,eax
-	call popfunc
-	mov ecx,eax
-	mov eax,ebx
-	call pushs
-	mov eax,ecx
-	call pushs
-
-exit_exchange:
-	pop ecx
-	pop ebx
-	pop eax
-	ret
-exchs ENDP
-
-;
-;neg macro
-;
-negs PROC
-push eax
-mov eax,0
-call popfunc
-neg eax
-call pushs
-pop eax
-ret
-negs ENDP
-
-
-rollu PROC
-;
-;roll up procedure
-;
-pushad  ;save all
-	
-	cmp shead, 0    ;1 or no data
-	jle RolluFin
-
-	;save the top
-	mov edi, shead
-	imul edi,4
-	mov edx, dstack[edi]
-	push edx
-	
-	imul ecx, shead, 4
-	mov eax, 28			;next spot
-	mov edi, 28           ;old spot
-RolluL: nop
-	sub edi, 4 ;get next element
-	cmp edi, -4             ;check bounds 
-	je AddbU         
-	mov ebx, dstack[edi] 
-	mov dstack[eax], ebx   ;move element down 1
-	sub eax, 4            ;up location
-	jmp RolluL
-
-AddbU: nop
-	pop edx
-	mov edi,0 ; put bottom back
-	mov dstack[edi], edx
-	;clear top crap
-	mov edi, shead
-	mov eax, edi
-	inc eax
-	imul eax,4
-	mov edi,eax
-	mov dstack[edi],0
-
-RolluFin: nop
-	 popad ;popall
-	ret
-rollu ENDP
-
-;
-;roll down
-;
-rolld PROC
-	pushad  ;save all
-	
-	cmp shead, 0    ;1 or no data
-	jle RolldFin
-
-	;save the bottom
-	mov edi, 0
-	mov edx, dstack[edi]
-	mov eax, 0           ;previous spot
-RolldL: nop
-	add edi, 4 ;get next element
-	cmp edi, 32             ;check bounds 
-	je AddbL         
-	mov ebx, dstack[edi] 
-	mov dstack[eax], ebx   ;move element down 1
-	add eax, 4            ;up location
-	jmp RolldL
-AddbL: nop
-	mov eax, shead
-	;sub eax, 1
-	imul eax, 4
-	mov edi, eax   
-	;sub edi, 4
-	mov dstack[edi],edx;put back
-RolldFin: nop
-	 popad ;popall
-	ret
-rolld ENDP
-
-;
-;view stack macro
-;prints out the contents of the stack
-;
-views PROC
-	push eax
-	push esi
-
-	mov esi,0
-PopStack:
-	mov eax, esi
-	imul eax,STACKDATASIZE		;calculate for the next esi result in eax
-	mov eax,dstack[eax]			;grab the contents at the index specified
-	call WriteInt				;display the contents of the stack
 	call Crlf
-	inc esi
-	cmp esi,8					;are we at the end of the stack
-	jl PopStack
-
-	pop esi
-	pop eax
-	ret
-views ENDP
-
-;
-;clear stack macro
-;
-clears PROC
-	push eax
-	push esi
-
-	mov esi,0
-ClrStack:
-	mov eax,esi
-	imul eax,STACKDATASIZE		;calculate for the next esi result in eax
-	mov dstack[eax],0			;grab the contents at the index specified
-	inc esi
-	cmp esi,8					;are we at the end of the stack
-	jl ClrStack
-	mov shead,-1				;reset the stack index
-
-	pop esi
-	pop eax
-	ret
-clears ENDP
-
-;
-;stahp procedure
-;empty procedure for a placeholder for the exit function
-;
-stahp PROC
-stahp ENDP
-
-;
-;dispError
-;displays that an error was encountered during processing
-;
-dispError PROC
-	push edx
-	mov edx,offset promptError
-	call WriteString
-	pop edx
-dispError ENDP
-
-
-;ckEqual
-;checks to see if a value, in eax, is equal to the value inputted
-ckEqual MACRO char, func
-  LOCAL nit
-  cmp eax,char
-  jnz nit
-  call func
-  call main
-  nit:
-ENDM
-
-;check if an ascii value is a valid operation
-;valid operations: +,-,*,/,X,N,U,D,V,C,Q
-;input is in eax
-;@return value in eax if value is digit, 2 in ebx if not a number
-checkOp PROC
-push edi
- ;get top of buffer
-  mov edi,0
-  movzx eax, buffer[edi]
-;check for '+'
-  ckEqual '+', adds
-;check for '-'
-  ckEqual '-', subs
-;check for *
-  ckEqual '*', muls
-;check for /
-  ckEqual '/', divs
-;check for 'X' or 'x'
-  ckEqual 'X', exchs
-  ckEqual 'x', exchs
-;check for 'N' or 'n'
-  ckEqual 'N', negs
-  ckEqual 'n', negs
-;check for 'U' or 'u'
-  ckEqual 'U', rollu
-  ckEqual 'u', rollu
-;check for 'D' or 'd'
-  ckEqual 'D', rolld
-  ckEqual 'd', rolld
-;check for 'V' or 'v'
-  ckEqual 'V', views
-  ckEqual 'v', views
-;check for 'C' or 'c'
-  ckEqual 'C', clears
-  ckEqual 'c', clears
-
-  push edx
- ;check digit
-  mov edi, 0
-  mov edx, 0
-PNLOOP: nop
-  mov eax,0
-  mov al, buffer[edi]
-  mov ecx, eax
-  
-  ;check for number col
-  and ecx, 30h
-  cmp ecx, 30h
-  jg invalid
-  
-  ;check for number row
-  and eax, 0Fh
-  cmp eax, 9
-  jg invalid
-
-  ;arrrrr there be a digit!
-  mov ebx, 1
-
-  ;shift and add
-  imul edx, 10
-  add edx,eax
-  inc edi
-  cmp edi, charred
-  jl PNLOOP
-  mov eax, edx
-  pop edx
-  jmp fin
-invalid:
-  mov ebx,2
-  jmp fin
-valid:
-  mov ebx,0
-fin:
-  
-  call WriteInt
-  pop edi
-  ret
-checkOp ENDP
-
+	jmp MainStart					;infinite loop until user types 'quit'
+main ENDP
 END main
