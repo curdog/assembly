bfb570c264872db4d3cce994c0b1eac746b69ab1
John Kirschner <kir2898@calu.edu>
2015-03-10 18:53:30 -0400

    Menu system
    Got the menu system work correctly. Laid the foundation for procedure calls for all of the functions.

--- G5P3.asm
+++ G5P3.asm
@@ -12,14 +12,7 @@ ExitProcess PROTO, dwExistCode:DWORD
 .data
 	CR equ 0Dh
 	LF equ 0Ah
-	cmdQuit byte "quit",0
-	cmdHelp byte "help",0
-	cmdLoad byte "load",0
-	cmdRun byte "run",0
-	cmdKill byte "kill",0
-	cmdShow byte "show",0
-	cmdStep byte "step",0
-	cmdChange byte "change",0
+	flag byte 1 dup(0)
 	inBuffer byte 30 dup(0)
 	helpMenu byte "Here are commands ,their parameters, and what they do",CR,LF,
 		"Quit: quits the program",CR,LF,
@@ -35,7 +28,6 @@ ExitProcess PROTO, dwExistCode:DWORD
 	errorMsgInput byte "You have entered an invalid command",CR,LF,0
 .code
 ;macros
-
 ;
 ;print
 ;takes in a string literal and prints it out
@@ -49,7 +41,6 @@ print MACRO text
 	mov edx, offset str
 	call WriteString
 ENDM
-
 ;
 ;println
 ;takes in a string literal and prints it out along with a CR and LF
@@ -63,14 +54,14 @@ println MACRO text
 	mov edx, offset str
 	call WriteString
 ENDM
-
 ;
 ;normStr
 ;converts all characters in the string to lowercase
 ;
 normStr MACRO str
 	push edi
-mov edi, edx
+	;mov edi,edx
+	mov edi, offset str
 	dec edi			;decrement because the first instruction is increment
 normCont:
 	inc edi
@@ -82,40 +73,67 @@ normCont:
 	je normFin
 	jmp normCont		;you aren't finished processing the string and need to continue
 normMask:
-	add byte ptr [edi],' '
+	add byte ptr [edi],' '			;convert character to lower case
 	jmp normCont
 normFin:
 	pop edi
 ENDM
 
 ;
-;cmpString macro
-;compares two strings zf = 0 equal zf = 1 not equal
-;strings must be null term
-; esi - str1 base
-; edi - str2 base
-;WARNING: function contains multiple exit points
+;cmpStrFunc
+;compares two strings, and if they are equal then executes the function passed to the macro
+;Note: second string is a string literal
+;@str1 the first string to compare
+;@str2 the second string to compare
+;@func the function that is to be executed if they come out equal
+;
+cmpStrFunc MACRO str1,str2,func
+	LOCAL text
+	LOCAL notEqual			;label doesn't work if you don't declare it as a local variable
+	.data
+	text byte str2,0		;truncate string literal with a 0
+	.code
+	mov edi, offset text
+	mov esi, offset str1
+	call cmpString
+	cmp eax,1
+	jne notEqual
+	mov flag,1				;the function was executed, so set the flag
+	call func
+notEqual:
+	nop
+ENDM
+
+;
+;PROCEDURES
+;
+
+;
+;cmpString procedure
+;NOTE: I moved around cmp ah,NULL & cmp ah,al switch them to do an absolute compare
+;compares two strings for equality
+;@return eax=1 if equal, eax=0 if not equal
+;strings must be null terminated
+;esi - offset str1
+;edi - offset str2
 cmpString PROC
-	pushad
 Start:	nop
-	mov al, [esi]
-	mov ah, [edi]
-	cmp ah, al		;compare chars
-	jne NotEqu		;quit early NOTE: will quit if one is NULL and other not
+	mov al, byte ptr [esi]
+	mov ah, byte ptr [edi]
 	cmp ah, NULL		;end of string and equal
-	je Equal		
+	je Equal
+	cmp ah, al			;compare chars
+	jne NotEqu			;quit early NOTE: will quit if one is NULL and other not		
 	inc esi
 	inc edi
 	jmp Start
 	
-NotEqu: nop 			;equal case
-	popad
-	;sez
-	ret
-	
-Equal:	nop			;not equal case
-	popad
-	;clz
+NotEqu: nop 			;not equal case 
+	mov eax,0
+	jmp cmpStringFin
+Equal:	nop				;equal case
+	mov eax,1
+cmpStringFin:
 	ret
 	
 cmpString ENDP
@@ -123,10 +141,23 @@ cmpString ENDP
 ;
 ;switchCmd
 ;a switch case like statement that compares the
-;input buffer to valid commands
+;inBuffer to valid commands
 ;
 switchCmd PROC
-	;check for exit
+	mov flag,0
+	cmpStrFunc inBuffer,"quit",exitProgram		;check for exit
+	cmpStrFunc inBuffer,"help",dispHelp			;check for help
+	cmpStrFunc inBuffer,"load",cmdLoad			;check for load
+	cmpStrFunc inBuffer,"run",cmdRun			;check for run
+	cmpStrFunc inBuffer,"kill",cmdKill			;check for kill
+	cmpStrFunc inBuffer,"show",cmdShow			;check for show
+	cmpStrFunc inBuffer,"step",cmdStep			;check for step
+	cmpStrFunc inBuffer,"change",cmdChange		;check for change
+	cmp flag,0
+	jne switchCmdFin							;falls through if none of the commands were executed
+	println "Invalid command entered, enter 'help' for a list of available commands"
+switchCmdFin:
+	ret
 switchCmd ENDP
 
 ;
@@ -155,30 +186,64 @@ dispHelp PROC
 dispHelp ENDP
 
 ;
-;dispErrorMsg
-;displays a general purpose error message
+;cmdLoad
 ;
-dispErrorMsg PROC
-	push edx
-	mov edx,offset errorMsg
-	call WriteString
-	pop edx
+cmdLoad PROC
+	println "cmdLoad placeholder"
 	ret
-dispErrorMsg ENDP
+cmdLoad ENDP
+
+;
+;cmdRun
+;
+cmdRun PROC
+	println "cmdRun placeholder"
+	ret
+cmdRun ENDP
+
+;
+;cmdKill
+;
+cmdKill PROC
+	println "cmdKill placeholder"
+	ret
+cmdKill ENDP
+
+;
+;cmdShow
+;
+cmdShow PROC
+	println "cmdShow placeholder"
+	ret
+cmdShow ENDP
+
+;
+;cmdStep
+;
+cmdStep PROC
+	println "cmdStep placeholder"
+	ret
+cmdStep ENDP
+
+;
+;cmdChange
+;
+cmdChange PROC
+	println "cmdChange placeholder"
+	ret
+cmdChange ENDP
 
 ;
 ;main procedure
 ;
 main PROC
 MainStart:
-	call dispHelp
+	print ": "						;prompt the user for entry
 	mov edx, OFFSET inBuffer		;set everything up for ReadString
 	mov ecx, sizeof inBuffer
 	call ReadString
 	normStr inBuffer				;normalize the inBuffer to all lowercase letters
-	mov edx, offset inBuffer
-	call WriteString
-	call Crlf
+	call switchCmd					;check for the input of a command
 	jmp MainStart					;infinite loop until user types 'quit'
 main ENDP
 
