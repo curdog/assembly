16b43140990c77cc5f5d452260232d69d7da220e
John Kirschner <kir2898@calu.edu>
2015-04-21 10:56:11 -0400

    menu system
    added menu system

--- G5P4.asm
+++ G5P4.asm
@@ -8,45 +8,24 @@
 
 Include Irvine32.inc
 .data
+;general data
+buffer		byte	25	 dup(0)		;used for input
+dest		byte	1	 dup(0)		;destination node
+source		byte	1	 dup(0)		;source node
+char		byte	2	 dup(0)		;used for temporarily printing out character values
+msg			byte	20	 dup(0)		;holds the transmission message
 
+;
 ;message structure
-QUEUE_DEST 	equ 0	;message destination				
-QUEUE_SRC	equ 1	;message source
-QUEUE_ORG	equ 2	;message origin (last touched)
-QUEUE_REC	equ 3	;message received flag 0-no  AOV-yes 
-QUEUE_TTL	equ 4	;Time to Live (and let die)
-QUEUE_MSG_S	equ 5	;Size of message (constant for now, place holder for awesome)
-QUEUE_MSG_V equ 6	;Message value
-QUEUE_SS 	equ 10	;total message size (for now)
-		
-;node structure
-;note: fixed length members should go first, calculate rest
-SIZE_C 		equ 0						;size of node (max total bytes is 16kb)
-NAME_C 		equ 2						;name of node
-DQUEUE_C	equ 3						;end index of queue
-EQUEUE_C 	equ 4						;start index of queue
-QUEUE_C 	equ 5 						;set size to 10 to start
-QUEUE_N		equ 10
-QUEUE_S 	equ QUEUE_N * QUEUE_SS	;queue 
-RXARRPTR_C 	equ QUEUE_C + QUEUE_S		;array pointer for rx queue
-RXARRPTR_S 	equ RXARRPTR_C + 4			;size of rx array
-TXARRPTR_C 	equ RXARRPTR_S + 1			;array pointer of pointers for tx queue
-TXARRPTR_S 	equ TXARRPTR_C + 4			;size of tx array
-CONNS_C 	equ TXARRPTR_S + 1			;string array of connection names
-DATA_C 		equ CONNS_C + 4				;start of the data portion
-;data is a bunch of bytes allocated after this data
-;formula for node size
-;size = 25 + QUEUE_S * QUEUE_SS + (n * 12) : where n is number of nodes (for perfect networks)
-;node pointers
-
-;4 nodes to start, all connected
-;              |---------------formula-------------| * #nodes
-nodesptr byte  1000     dup(0)
-nodesptr_s equ 1000
-;node
+QUEUE_DEST 		equ 0	;message destination				
+QUEUE_SRC		equ 1	;message source
+QUEUE_ORG		equ 2	;message origin (last touched)
+QUEUE_TTL		equ 4	;Time to Live (and let die)
+QUEUE_MSG_SIZE	equ 5	;Size of message (constant for now, place holder for awesome)
+QUEUE_MSG		equ 6	;Message value
+QUEUE_SS 		equ 12	;total message size (for now)
 
 ;=======Strings=======
-char		byte 2 dup(0)		;used for temporarily printing out character values
 welcome_msg byte "Welcome to the Nodetrix!!!",0
 bye_msg     byte "Congrats on taking the blue pill",0
 file_msg	byte "Enter File Name",0
@@ -72,8 +51,8 @@ nodes byte 2000 dup(0)
 name			equ		0		;offset of the name					size: 1 byte
 connections		equ		1		;offset of the connections			size: 1 byte
 txqueue			equ		2		;offset of the txqueue				size: 4 bytes
-inPtr			equ		6		;offset of the inPtrtxqueue			size: 4 bytes
-outPtr			equ		10		;offset of the outPtrtxqueue		size: 4 bytes
+inPtr			equ		6		;offset of the inPtr				size: 4 bytes
+outPtr			equ		10		;offset of the outPtr				size: 4 bytes
 constNodesize	equ		14		;size of the constant space in each node
 ;variable sized structure data fields
 ;total size: 16 bytes
@@ -123,6 +102,17 @@ println MACRO text
 	mov edx, offset str
 	call WriteString
 	pop edx
+ENDM
+
+;
+;ckEqual
+;checks to see if a value, in eax, is equal to the value inputted
+ckEqual MACRO char, func
+  LOCAL nit
+  cmp eax,char
+  jnz nit
+  call func
+  nit:
 ENDM
 
 ;
@@ -431,6 +421,15 @@ varNodeSizeLoop:
 dispNodes ENDP
 
 ;
+;transmitMessage
+;transmits a message through the node system
+;
+transmitMessage PROC
+	println "Placeholder"
+	ret
+transmitMessage ENDP
+
+;
 ;dispConnections
 ;displays all the nodes and how they're connected to one another
 ;uses a kind of graphic display with ascii characters
@@ -447,6 +446,22 @@ dispConnections ENDP
 ;
 ;dispMenu
 ;displays the menu so that the user can choose a command to execute
+;
+dispMenu PROC
+	println "1. Display node connections"
+	println "2. Display node information"
+	println "3. Transmit a string from one node to another"
+	println "4. Quit"
+	ret
+dispMenu ENDP
+
+;
+;exitProgram
+;exits the program
+;in a function for ease of use
+exitProgram PROC
+	INVOKE ExitProcess,0
+exitProgram ENDP
 
 
 ;helper functions
@@ -634,18 +649,40 @@ logClose proc
 	ret
 logClose endp 
 
-;entry point
+;
+;switchMenu
+;a switch case menu kind of system
+;will be used in conjunction with the dispMenu to let
+;the user choose what action to perform
+;will be an ascii value of '1' to '4'
+switchMenu PROC
+	movzx eax,buffer
+	ckEqual '1',dispNodes			;display the node information for the user
+	ckEqual '2',dispConnections		;display the node connections for the user
+	ckEqual '3',transmitMessage		;transmit a message for the user
+	ckEqual '4',exitProgram			;exit the program for the user
+	ret
+switchMenu ENDP
+
+;
+;main procedure
+;executes the main loop of the program, is the spine of the program
+;
 main proc
-	call initNodes
-	call dispNodes
-	call logOpen
-	call dispConnections
-	
-	call txstep
-	call rxstep
-	
-	call logClose
-	call Waitmsg			;just wait up at the end for now
-	INVOKE ExitProcess,0
+	call initNodes			;initialize all the nodes
+	println " "				;throw an extra carriage return in there
+
+loopMain:
+	call dispMenu			;display the menu
+	print ": "				;prompt the user
+	mov edx,offset buffer
+	mov ecx,sizeof buffer
+	call ReadString			;read user input
+	mov eax,0
+	mov al,buffer			;move the character that buffer is pointing to into al to prep for switchMenu
+	call switchMenu			;call the procedure that is basically a switch menu
+	println " "				;print out a carriage return
+	jmp loopMain			;branch back up the mainLoop infinitely until user decides to exit
+
 main endp 
 END main
